0030e6e1c5878bf5fc0d71c324b0637e98e6955e
Fly me to the moon <git@bigly.dog>
2021-08-03 11:06:06 -0700

    loop cleanup

--- iso_cp/__main__.py
+++ iso_cp/__main__.py
@@ -1,16 +1,22 @@
-from asyncio import get_event_loop
+from asyncio import all_tasks, gather, get_event_loop
 
 from .main import main
 
 loop = None
 try:
     loop = get_event_loop()
-    code = loop.run_until_complete(main())
-except KeyboardInterrupt:
-    exit(130)
-else:
-    exit(code)
+    try:
+        code = loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        exit(130)
+    else:
+        exit(code)
 finally:
     if loop:
-        loop.close()
-
+        try:
+            tasks = all_tasks()
+            for task in tasks:
+                task.cancel()
+            loop.run_until_complete(gather(*tasks, return_exceptions=True))
+        finally:
+            loop.close()
--- iso_cp/shared.py
+++ iso_cp/shared.py
@@ -1,4 +1,5 @@
-from asyncio.subprocess import PIPE, create_subprocess_exec
+from asyncio.subprocess import DEVNULL, PIPE, create_subprocess_exec
+from contextlib import suppress
 from shlex import quote
 from typing import Iterable, Optional, cast
 
@@ -8,7 +9,15 @@ def join(cmds: Iterable[str]) -> str:
 
 
 async def call(prog: str, *args: str, stdin: Optional[bytes] = None) -> int:
-    proc = await create_subprocess_exec(prog, *args, stdin=PIPE)
-    await proc.communicate(stdin)
-    return cast(int, proc.returncode)
+    proc = await create_subprocess_exec(
+        prog,
+        *args,
+        stdin=PIPE if stdin else DEVNULL,
+    )
+    try:
+        await proc.communicate(stdin)
+    finally:
+        with suppress(ProcessLookupError):
+            await proc.wait()
 
+    return cast(int, proc.returncode)
